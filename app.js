264876fef58bc1b5416abcd5877cf23ca15236fa
Joseph Wells <josephwells@Josephs-MacBook-Pro.local>
2017-03-26 09:12:05 -0700

    Init commit of local repo

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
+//setting the default view engine
+
+app.set('views', './views');
+app.set('view engine','pug');
+
+//express will serve up the static domains
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images',express.static(path.join(__dirname, 'uploads')));
+
+/*
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+*/
+
+app.get('/', function(req, res){
+  var filesArray = fs.readdirSync('uploads');
+  filesArray.shift(); //Used to remove the .DS_Store created on MacOS //REMOVE THIS BEFORE UPLOADING TO CLOUD
+  filesArray.reverse();
+  console.log(filesArray);
+  res.render('index', {
+    files: filesArray
+  });
+});
+
+app.post('/upload', function(req, res){
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, Date.now().toString())); //path.join(form.uploadDir, file.name)
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});
+
+var server = app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
